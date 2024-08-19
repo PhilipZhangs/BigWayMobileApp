b05d5840516f34caff5ed5c23c467e367c11e97a
PhilipZhangs <zhangsphilip@gmail.com>
2024-08-19 02:21:56 -0700

    Initial Commit

---
+++ BigWayMobileApp/BigWayMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigWayMobileAppApp.swift
+//  BigWayMobileApp
+//
+//  Created by 张胖子 on 2024-08-19.
+//
+
+import SwiftUI
+
+@main
+struct BigWayMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigWayMobileApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BigWayMobileApp
+//
+//  Created by 张胖子 on 2024-08-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
